746bede0ac9a74f1ad98c71317f423f1c7c47945
opt <opt@chipmunk.land>
2026-04-01 00:05:23 +0000

    Update to 26.1

--- src/main/java/de/florianmichael/dstathud/mixin/MixinConnection.java
+++ src/main/java/de/florianmichael/dstathud/mixin/MixinConnection.java
@@ -18,24 +18,23 @@
 
 package de.florianmichael.dstathud.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import de.florianmichael.dstathud.NetworkTrafficHandler;
-import de.florianmichael.dstathud.StringUtils;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.debug.DebugHudLines;
-import net.minecraft.client.gui.hud.debug.TpsDebugHudEntry;
-import net.minecraft.util.Formatting;
+import io.netty.channel.ChannelPipeline;
+import net.minecraft.network.BandwidthDebugMonitor;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.PacketFlow;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TpsDebugHudEntry.class)
-public class MixinTpsDebugHudEntry {
-    @Inject(method = "render", at = @At(value = "RETURN", target = "Lnet/minecraft/client/gui/hud/debug/DebugHudLines;addLine(Ljava/lang/String;)V"))
-    public void showDStat(CallbackInfo ci, @Local(argsOnly = true) DebugHudLines lines) {
-        if (MinecraftClient.getInstance().isInSingleplayer()) return;
+@Mixin(Connection.class)
+public class MixinConnection {
 
-        lines.addLine(Formatting.GOLD + StringUtils.formatBytes(NetworkTrafficHandler.outgoing) + "/s Up, " + StringUtils.formatBytes(NetworkTrafficHandler.incoming) + "/s Down");
+    @Inject(method = "configureSerialization", at = @At("RETURN"))
+    private static void addNetworkStats(ChannelPipeline pipeline, PacketFlow inboundDirection, boolean local, BandwidthDebugMonitor monitor, CallbackInfo ci) {
+        if (inboundDirection == PacketFlow.CLIENTBOUND) {
+            pipeline.addFirst(NetworkTrafficHandler.NETWORK_TRAFFIC_HANDLER_NAME, new NetworkTrafficHandler());
+        }
     }
 }
--- src/main/java/de/florianmichael/dstathud/mixin/MixinDebugEntryTps.java
+++ src/main/java/de/florianmichael/dstathud/mixin/MixinDebugEntryTps.java
@@ -18,23 +18,23 @@
 
 package de.florianmichael.dstathud.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import de.florianmichael.dstathud.NetworkTrafficHandler;
-import io.netty.channel.ChannelPipeline;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.NetworkSide;
-import net.minecraft.network.handler.PacketSizeLogger;
+import de.florianmichael.dstathud.StringUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.debug.DebugEntryTps;
+import net.minecraft.client.gui.components.debug.DebugScreenDisplayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientConnection.class)
-public class MixinClientConnection {
-
-    @Inject(method = "addHandlers", at = @At("RETURN"))
-    private static void addNetworkStats(ChannelPipeline pipeline, NetworkSide side, boolean local, PacketSizeLogger packetSizeLogger, CallbackInfo ci) {
-        if (side == NetworkSide.CLIENTBOUND) {
-            pipeline.addFirst(NetworkTrafficHandler.NETWORK_TRAFFIC_HANDLER_NAME, new NetworkTrafficHandler());
-        }
+@Mixin(DebugEntryTps.class)
+public class MixinDebugEntryTps {
+    @Inject(method = "display", at = @At(value = "RETURN", target = "addLine"))
+    public void showDStat(CallbackInfo ci, @Local(argsOnly = true, name = "displayer") DebugScreenDisplayer displayer) {
+        if (Minecraft.getInstance().isSingleplayer()) return;
+        displayer.addLine(ChatFormatting.GOLD + StringUtils.formatBytes(NetworkTrafficHandler.outgoing) + "/s Up, " + StringUtils.formatBytes(NetworkTrafficHandler.incoming) + "/s Down");
     }
 }
